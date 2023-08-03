a00a84835cc8f2c39c285e525b68e955c6ba2273
Roman Volosatovs <rvolosatovs@riseup.net>
2023-08-04 00:00:36 +0200

    build(nix): define binary caches in the flake
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- flake.nix
+++ flake.nix
@@ -1,4 +1,15 @@
 {
+  nixConfig.extra-substituters = [
+    "https://bytecodealliance.cachix.org"
+    "https://nix-community.cachix.org"
+    "https://cache.garnix.io"
+  ];
+  nixConfig.extra-trusted-public-keys = [
+    "bytecodealliance.cachix.org-1:0SBgh//n2n0heh0sDFhTm+ZKBRy2sInakzFGfzN531Y="
+    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+  ];
+
   inputs.fenix.url = github:nix-community/fenix;
   inputs.nixify.inputs.nixpkgs.follows = "nixpkgs";
   inputs.nixify.url = github:rvolosatovs/nixify;
